e749542e58e4fe56e0be9a49b4c13dfd961e7e16
Milosz Linkiewicz <milosz.lin@gmail.com>
2020-04-04 08:03:24 +0200

    Finished writting more flexible REST api connector.

---
+++ app/src/main/java/com/votedesk/data/model/SingleProjectExtended.java
@@ -0,0 +1,11 @@
+package com.votedesk.data.model;
+
+public class SingleProjectExtended extends SingleProject {
+
+    SingleProjectExtended() {
+    }
+
+    SingleProjectExtended(SingleProject basicData) {
+        super(basicData);
+    }
+}
